3da42cf46dc088babb24a502643737b4cb9caa34
Marc Cavazos <marcavazos19@icloud.com>
2022-01-11 17:28:27 -0500

    commit tree 63a5b529454970f4b216905002eeff9572760b37

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'some bob';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'some other bob';
+    return leastFavoriteCustomer;
+}
